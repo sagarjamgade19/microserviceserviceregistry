086e1241ba41ef05cdccdfc6e0f7259f8937a7bf
sagar <sjamgade19@gmail.com>
2023-05-12 15:47:41 +0530

    first commit Service Registry

---
+++ src/test/java/com/codesdaily/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesdaily.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
